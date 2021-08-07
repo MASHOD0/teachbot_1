9f2e62f0ecff1c78f918e4cb6bf30cbcd16a39ad
MASHOD0 <mashhood2002@gmail.com>
2021-08-08 01:05:43 +0530

    bag of words

--- utils.py
+++ utils.py
@@ -21,5 +21,21 @@ def stem(word):
     :param word: word to stem
     :return: stemmed word
     """
+    stemmer = PorterStemmer()
     return stemmer.stem(word.lower())
 
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    
+    """
+
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
